de196aae89100037da7731b0331a461aab3b13ac
Manish Rathi <manishrathi4@gmail.com>
2020-07-16 19:12:05 +0530

    one deposit to account

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,7 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private long balance = 0;
+
     public long balance() {
-        return 0;
+        return balance;
+    }
+
+    public long deposit (long amount) {
+        balance = amount;
+        return balance;
     }
 }
